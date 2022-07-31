81f9fbec3df2d73cf47105c64d7b5d6ca63ba217
Erikson <chris.s.erickson@gmail.com>
2022-07-31 18:31:35 -0400

    final commit

--- app.js
+++ app.js
@@ -87,8 +87,9 @@
 // });
 
 // streamline code after 9.2
-const fs = require('fs');
+// const fs = require('fs'); commented out after adding the generate-site which also has fs
 const inquirer = require('inquirer');
+const {writeFile, copyFile} = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -215,14 +216,43 @@ Add a New Project
     });
 };
 
+// promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+//     console.log(portfolioData);
+//     // will be uncommented in lesson 4
+//     const pageHTML = generatePage(portfolioData);
+//     fs.writeFile('./dist/index.html', pageHTML, err => {
+//       if (err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Page created! Check out index.html in this directory to see it!');
+    
+//       fs.copyFile('./src/style.css', './dist/style.css', err => {
+//         if (err) {
+//           console.log(err);
+//           return;
+//         }
+//         console.log('Style sheet copied successfully!');
+//       });
+//     });
+//   });
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
